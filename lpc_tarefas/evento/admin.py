5bd2b088249713ab3e22c0df210aef3328654a62
lucasbastoos <lucas.alves.lab@gmail.com>
2017-06-12 15:42:42 -0300

    Add files via upload

---
+++ lpc_tarefas/evento/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from evento.models import *
+
+
+admin.site.register(Usuario)
+admin.site.register(Projeto)
+admin.site.register(ProjetoUsuario)
+admin.site.register(Tarefa)
---
+++ lpc_tarefas/evento/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User, AbstractUser
+
+class Usuario(User):
+    print (User)
+    def __str__(self):
+        return '{}:{}'.format(self.nome, self.email)
+
+class Projeto(models.Model):
+    nome = models.CharField('nome', max_length=200)
+
+    def __str__(self):
+        return '{}'.format(self.nome)
+
+
+class ProjetoUsuario(models.Model):
+    usuario = models.ForeignKey('Usuario')
+    projeto = models.ForeignKey('Projeto')
+
+    def __str__(self):
+        return '{}'.format(self.usuario)
+
+
+class Tarefa(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    usuario = models.ForeignKey("auth.User")
+    projeto = models.ForeignKey('Projeto')
+    dataEHoraDeInicio = models.DateTimeField('dataEHoraDeInicio', default=timezone.now)
+
+    def __str__(self):
+        return '{}'.format(self.nome)
